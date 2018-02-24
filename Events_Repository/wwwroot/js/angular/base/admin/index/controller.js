1975623a52c78820ad42571b59794fe96a491344
kirill <kirill.myp@yandex.ru>
2018-02-25 02:10:29 +0300

    start filling pages and js this pages

---
+++ Events_Repository/wwwroot/js/angular/base/admin/index/controller.js
@@ -0,0 +1,69 @@
+﻿eveApp.controller('AdminIndexController', function ($scope, $http) {
+
+    $scope.urls = {
+        getOrganizations:"",
+        getRoles:"",
+        getCountries:"",
+        getCities:"",
+        getTags:"",
+        getTickets:"",
+        getUsers:""
+    }
+
+    $scope.filterTimer = null;
+    $scope.table = {
+        filter: {},
+        visibleTable: {
+            organizations: false,
+            roles: false,
+            countries: false,
+            cities: false,
+            tags: false,
+            tickets: false,
+            users: false
+        },
+        items: [],
+        lastUrl: "",
+        cleanTableAddiction: () => {
+            $scope.table.filter = {};
+        }
+    }
+
+    $scope.getOrganizations = () => {
+        updateItems($scope.urls.getOrganizations, $scope.table.items, $scope.table.filter);
+    }
+    $scope.getRoles = () => {
+        updateItems($scope.urls.getUsers, $scope.table.items, $scope.table.filter);
+    }
+    $scope.getCountries = () => {
+        updateItems($scope.urls.getCountries, $scope.table.items, $scope.table.filter);
+    }
+    $scope.getCities = () => {
+        updateItems($scope.urls.getCities, $scope.table.items, $scope.table.filter);
+    }
+    $scope.getTags = () => {
+        updateItems($scope.urls.getTags, $scope.table.items, $scope.table.filter);
+    }
+    $scope.getTickets = () => {
+        updateItems($scope.urls.getTickets, $scope.table.items, $scope.table.filter);
+    }
+    $scope.getUsers = () => {
+        updateItems($scope.urls.getUsers, $scope.table.items, $scope.table.filter);
+    }
+
+    function updateItems(url, items, filter) {
+        $http.get(url, filter).then((request) => {
+            if (request.success) {
+                angular.copy(request.data, items);
+            }
+        });
+    }
+
+    $scope.filterDelayed = function () {
+        if ($scope.filterTimer) {
+            clearTimeout($scope.filterTimer);
+            $scope.filterTimer = null;
+        }
+        $scope.filterTimer = setTimeout($scope.table.lastUrl, 1000);//todo i think, here need to binding to a context
+    };
+})
--- Events_Repository/wwwroot/js/angular/base/login/index/controller.js
+++ Events_Repository/wwwroot/js/angular/base/login/index/controller.js
@@ -1,9 +1,51 @@
-﻿eveApp.controller('FirstController', function ($scope, $http) {
-    $scope.signInForm= { login: "", password: "" }
-    $scope.signUpForm= { login: "", email:"", password: "" }
+﻿eveApp.controller('LoginIndexController', function ($scope, $http) {
+    $scope.signInForm = { login: "", password: "" }
+    $scope.signUpForm = { login: "", email: "", password: "" }
     $scope.loginMode = true;
+    $scope.signIn = () => { $("#signIn").submit(); }
+    $('#signIn').on('submit', (e) => {
+        e.preventDefault();
+        var $form = $(this);
+        var url = $form.attr("action");
 
-    $scope.changeMode = function() {
+        if (!signInForm.login || !signInForm.password) {
+            $('.err_signIn').show();
+        }
+        var data = $scope.signInForm;
+
+        $http.post(url, data).then((request) => {
+            if (request.isSuccess) {
+                window.location = "/";
+            } else {
+                //showMessage("Something is going bad");
+            }
+            
+        });
+
+    });
+
+    $scope.signUp = () => { $("#signUp").submit(); }
+    $('#signUp').on('submit', (e) => {
+        e.preventDefault();
+        var $form = $(this);
+        var url = $form.attr("action");
+
+        if (!signInForm.login || !signInForm.password) {
+            $('.err_signIn').show();
+        }
+        var data = $scope.signInForm;
+
+        $http.post(url, data).then((request) => {
+            if (request.isSuccess) {
+                $scope.signInForm = $scope.signUpForm;
+                $scope.changeMode();
+            }
+            
+        });
+
+    });
+    $scope.ProcessSignInForm = () => { }
+    $scope.changeMode = () => {
         $scope.loginMode = !$scope.loginMode;
     }
 
@@ -11,7 +53,9 @@
 
 
 
-    $scope.init = function() {
+    function controller() {
 
     }
+
+    controller();
 });
